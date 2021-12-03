5c21995ecd68bdc4a1e719b709cc1e5ead4774b9
spfrijns <simon.frijns@outlook.com>
2021-12-03 12:03:46 +0100

    adding solutions for day-03

---
+++ day-03/code.py
@@ -0,0 +1,50 @@
+def get_input(filename: str) -> list:
+    with open(filename) as input:
+        input_list = input.read().strip().split("\n")
+        return input_list
+
+def part1(report:list) -> int:
+    ans = 0
+    gamma = epsilon = ""
+    pivoted_report = [list(i) for i in zip(*report)] 
+    for row in pivoted_report:
+        if row.count('0') > row.count('1'):
+            gamma += '0'
+            epsilon += '1'
+        else:
+            gamma += '1'
+            epsilon += '0'
+    ans = int(gamma, 2) * int(epsilon, 2)
+    return ans
+
+def find_rating(report: list, column: int, gas: str) -> list or int:
+    if len(report) == 1:
+        return int(report[0], 2)
+    pivoted_report = [list(i) for i in zip(*report)]
+    row = pivoted_report[column]
+    match gas:
+        case 'co2': 
+            digit = '0' if row.count('1') >= row.count('0') else '1'
+        case 'oxygen':
+            digit = '1' if row.count('1') >= row.count('0') else '0'
+    report = [row for row in report if row[column] == digit]
+    column += 1
+    return find_rating(report, column, gas)
+
+def part2(report: list) -> int:
+    ans = 0
+    oxygen = find_rating(report, 0, 'oxygen')
+    co2 = find_rating(report, 0, 'co2')
+    ans = oxygen * co2
+    return ans
+    
+def test():
+    test_input = get_input("test_input.txt")
+    assert part1(test_input) == 198
+    assert part2(test_input) == 230
+
+if __name__ == "__main__":
+    test()
+    input = get_input("input.txt")
+    print(part1(input))
+    print(part2(input)) 
